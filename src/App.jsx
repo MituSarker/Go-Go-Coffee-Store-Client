ef34203c5fd1d3c0e754e164695f222d69abf8e2
MituSarker <mitusarkar401@gmail.com>
2023-09-30 00:07:17 +0600

    add delete oparetion

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@
 import { useLoaderData } from 'react-router-dom'
 import './App.css'
 import CoffeeCard from './components/CoffeeCard';
+import { useState } from 'react';
 
 function App() {
-  const coffees=useLoaderData();
-
+  const loadedCoffees=useLoaderData();
+  const [coffees, setCoffees] = useState(loadedCoffees);
   return (
     <>
     
@@ -15,8 +16,8 @@ function App() {
           coffees.map(coffee => <CoffeeCard
             key={coffee._id}
             coffee={coffee}
-           // coffees={coffees}
-           // setCoffees={setCoffees}
+            coffees={coffees}
+           setCoffees={setCoffees}
           ></CoffeeCard>)
         }
       </div>
